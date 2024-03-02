f87940cca7ff12d10cc0f8975e653667c45a8f49
Vichiro <vicente.sepulvedar@utem.cl>
2024-03-02 20:20:14 -0300

    test 2

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from app_attendward.controladores import controlador_secciones,controlador_asistencias,controlador_docentes
 from app_attendward import app
 
-if __name__ == "__main__": #ola
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
